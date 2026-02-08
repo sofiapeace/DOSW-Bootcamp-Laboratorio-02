78c26cabf53c6d6320632ddd4599e573ce5f7f5d
sofiapeace <ginasofia0227@gmail.com>
2026-02-08 15:42:38 -0500

    Estructura inicial laboratorio 2

---
+++ src/main/java/edu/dosw/lab/Application.java
@@ -0,0 +1,6 @@
+package edu.dosw.lab;
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Maven configurado y corriendo correctamente");
+    }
+}
---
+++ src/test/java/edu/dosw/bootcamp/lab/DOSW_Bootcamp_Laboratorio_02/DoswBootcampLaboratorio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.bootcamp.lab.DOSW_Bootcamp_Laboratorio_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswBootcampLaboratorio02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
